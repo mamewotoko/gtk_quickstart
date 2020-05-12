6146bfacf2ee46ff85f62c36b45a4d33423aef53
Takashi Masuyama <mamewotoko@gmail.com>
2020-05-12 20:11:41 +0900

    modify path of mac to use make

--- env.sh
+++ env.sh
@@ -1,7 +1,8 @@
 export UNAME="$(uname)"
 
-
-if [[ "$UNAME" == "MSYS_NT"* ]]; then
-   export PATH=/mingw64/bin:$PATH
-   export PKG_CONFIG_PATH=/mingw64/lib/pkgconfig
+if [ "$UNAME" = Darwin ]; then
+    export PATH="/usr/local/opt/make/libexec/gnubin:$PATH"
+elif [[ "$UNAME" == "MSYS_NT"* ]]; then
+    export PATH=/mingw64/bin:$PATH
+    export PKG_CONFIG_PATH=/mingw64/lib/pkgconfig
 fi     
